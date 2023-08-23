5ef89c9cc2593b6d5df99ab110c05c610865f2a3
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2023-08-23 16:10:28 +0530

    2dmatrixcalc.c

--- 2dmatrixcalc.c
+++ 2dmatrixcalc.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int a[2][2],b[2][2],c[2][2],i,n,j;
+	int a[2][2],b[2][2],c[2][2],k,i,n,j;
 	
 	printf("\n enter the value first matrix:");
 	for(i=0;i<2;i++)
@@ -60,6 +60,26 @@ int main()
 		for(j=0;j<2;j++)
 		{
 			c[i][j]=a[i][j]-b[i][j];
+		}
+	}					 
+	for(i=0;i<2;i++)   
+	{				
+		for(j=0;j<2;j++)
+		{
+			printf("\t%d",c[i][j]);
+		}
+	    printf("\n");
+	}
+	printf("\n\n multiplication of matrix:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			c[i][j]=0;
+			for(k=0;k<2;k++)
+			{
+				c[i][j] += a[i][k]*b[k][j];
+			}
 		}
 	}					 
 	for(i=0;i<2;i++)   
