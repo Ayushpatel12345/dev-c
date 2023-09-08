1fc02a3f9cd007a0abd6a77dda017f4303655a2e
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2023-09-08 16:46:29 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -4,16 +4,21 @@ int main()
 	int i,n,a=0;
 	printf("\n enter the number:");
 	scanf("%d",&n);
-	for(i=2;i<=n-1;i++)
+	for(i=2;i<=n/2;i++)
 	{
 		if(n%i==0)
 		{
-				printf("number is not prime");
-				a=1;
-				break;		
+			a++;
+			break;
 		}
 	}
-	if(a==0)
-	printf("\n number is prime");
+	if(a==0 && n!=1)
+	{
+		printf("\n prime number");
+	}
+	else
+	{
+		printf("\n prime not number");
+	}
 	return 0;
 }
